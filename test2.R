3f0d1789ec864430623fb19b3020489fc3890fc1
Zelazny7 <gravcon5@gmail.com>
2018-02-02 17:34:42 -0600

    fixing case sensitive folder name rubbish

--- test2.R
+++ test2.R
@@ -4,14 +4,14 @@ x <- titanic$Fare
 y <- titanic$Survived
 
 
-opts <- discretizer_options("iv", max.bin=12, min.res=10, min.cnt=25, epsilon=0.1)
+opts <- discretizer_options("iv", max.bin=12, mono=1,  min.res=10, min.cnt=25, epsilon=0.01)
 disc <- Information_Value_Discretizer$new(opts)
-
 
 x <- titanic$Age
 disc$fit(x, y)
 disc$breaks
 
-table(cut(x, disc$breaks, right = FALSE), y, useNA = "ifany")
+res <- table(cut(x, disc$breaks, right = FALSE), y, useNA = "ifany")
+z <- cbind(res, res[,2]/rowSums(res))
+barplot(z[,3], horiz=TRUE)
 
-findInterval(1:10, 5:7, rightmost.closed = FALSE)
